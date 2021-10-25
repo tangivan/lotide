ee02a74f56f0cdf9034f1b5787f701861b7a3827
Ivan <ivanxintang@gmail.com>
2021-10-25 14:04:27 -0700

    Implemented assertEqual function of lotide

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected)
+    console.log(`🛑🛑🛑 Assertion Passed: [${actual}] === [${expected}]`);
+  else
+    console.log(`✅✅✅ Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+assertEqual("identical string", "identical string");
+assertEqual("non identical string", "NON IDENTICAL STRING");
+assertEqual(25, 25);
+assertEqual(1, 2);
