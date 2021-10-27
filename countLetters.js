a8bd5bc9e24d1ac26c1a6b5d55b59cb6774cbe82
Ivan <ivanxintang@gmail.com>
2021-10-27 14:51:03 -0700

    implemented countLetters of lotide project

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  }
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countLetters = (str) => {
+  let results = {};
+  const strWithNoSpaces = str.split(" ").join("");
+  for (const char of strWithNoSpaces) {
+    if (results[char])
+      results[char]++;
+    else
+      results[char] = 1;
+  }
+  return results;
+}
+
+let result = countLetters('lighthouse in the house');
+assertEqual(result["l"], 1);
+assertEqual(result["i"], 2);
+assertEqual(result["h"], 4);
+assertEqual(result["n"], 1);
+let resultTwo = countLetters('aaabbbccc');
+assertEqual(resultTwo["a"], 3);
+assertEqual(resultTwo["b"], 3);
+assertEqual(resultTwo["c"], 3);
+let resultThree = countLetters('');
+assertEqual(resultThree[''], undefined);
+let resultFour = countLetters('a');
+assertEqual(resultFour["a"], 1);
