f8068cb37fbd31c3a02f35d58955866237dd021e
Ivan <ivanxintang@gmail.com>
2021-10-27 17:12:17 -0700

    implemented findKeyByValue function of the lotide project

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  }
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKeyByValue = (object, str) => {
+  for (const key in object) {
+    if (object[key] === str) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
