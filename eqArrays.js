ea490d169338f5243418fa51acdf77d4eef03544
Ivan <ivanxintang@gmail.com>
2021-10-29 17:37:16 -0700

    Implemented support for nested arrays in eqArrays assertion

--- eqArrays.js
+++ eqArrays.js
@@ -5,22 +5,42 @@ const assertEqual = (actual, expected) => {
     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
+// const eqArrays = (arr1, arr2) => {
+//   let assert = false;
+//   if (arr1.length === 0 && arr2.length === 0) return true;
+//   if (arr1.length === arr2.length) {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] === arr2[i]) {
+//         assert = true;
+//       }
+//       else {
+//         assert = false;
+//       }
+//     }
+//   }
+//   return assert;
+// };
+
 const eqArrays = (arr1, arr2) => {
-  let assert = false;
   if (arr1.length === 0 && arr2.length === 0) return true;
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i])
-        assert = true;
-      else
-        assert = false;
+  if (!(arr1.length === arr2.length)) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) return false;
+    } else {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
   }
-  return assert;
-};
+  return true;
+}
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
